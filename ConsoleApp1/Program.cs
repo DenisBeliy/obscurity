a0602535ddd79667a544dfa2be9ce57aff9a7b4f
Денис Белый <belyydv@gmail.com>
2021-10-25 23:05:16 +0300

    Сделал fullScreen

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,18 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 using ConsoleApp1.logo;
 namespace ConsoleApp1
 {
     class Program
     {
+        [DllImport("user32.dll")]
+        private static extern bool ShowWindow(System.IntPtr hWnd, int cmdShow);
+
+
         static void Main(string[] args)
         {
+            ConsoleFullScreen();
+            Thread.Sleep(500); 
             Greetings.PrintLogotips();
             Console.ReadLine();
+        }
+
+        private static void ConsoleFullScreen()
+        {
+            Process p = Process.GetCurrentProcess();
+            ShowWindow(p.MainWindowHandle, 3);
         }
     }
 }
--- ConsoleApp1/logo/Greetings.cs
+++ ConsoleApp1/logo/Greetings.cs
@@ -4,23 +4,24 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.IO;
-
+using System.Runtime.InteropServices;
+using System.Diagnostics;
 
 namespace ConsoleApp1.logo
 {
     
     class Greetings
     {
-        
+    
         public static void PrintLogotips()
         {
+          //  ConsoleFullScreen(); // консоль во весь экран
             PrintCompanyLogo(); // отрисовка логотипа компании
             PrintGameLogo(); // отрисовка логотипа игры
         }
 
 
-        
-
+      
         private static void PrintCompanyLogo()
         {
             string logoCompany = Properties.Resources.logo_company;
